b8f3c32d991fdc93ba1a46601d4e1dd1755b2498
Shavingmace <pskyd0151@gmail.com>
2018-03-20 15:16:27 +0900

    ch5 pt.1 클리어

---
+++ ch4/example.py
@@ -0,0 +1,21 @@
+index = 3
+typeOf(index) #int
+
+index = '3'
+
+index +5
+
+var index;
+
+
+int a;
+str b;
+
+print(a)
+
+print a
+
+print (a)
+0.0
+
+float int
---
+++ ch4/practice.py
@@ -0,0 +1,7 @@
+def f(seq):
+    result = []
+    for i in seq:
+        result.append(i*2)
+    return result
+
+print(f([1, '3', True]))
